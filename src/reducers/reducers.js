de91cdb6ac0fc8ec96b2c7e21326bbac36171b25
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-07-31 07:41:59 +0200

    added combineReducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -20,11 +20,16 @@ function movies(state = [], action) {
   }
 }
 
-function moviesApp(state = {}, action) {
+/*function moviesApp(state = {}, action) {
   return {
     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
     movies: movies(state.movies, action)
   }
-}
+} */
+
+const moviesApp = combineReducers({
+  visibilityFilter,
+  movies
+});
 
 export default moviesApp;
